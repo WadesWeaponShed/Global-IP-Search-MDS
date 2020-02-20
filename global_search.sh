eecd3aedb6fba64d34a147ea4937a863e1612c07
Adam Forester <aforester@silverdogstudios.com>
2020-02-20 08:51:54 -0600

    v2
    Updated with better search function and output file

--- global_search.sh
+++ global_search.sh
@@ -3,7 +3,8 @@ read IP
 
 mgmt_cli -r true show domains limit 500 --format json | jq --arg IP "$IP" -r '.objects[] | "mgmt_cli -r true -d " + .name + " show objects ip-only true filter " + $IP + " --format json" ' >> search.sh
 
-mgmt_cli -r true show mdss details-level full --format json | jq -r --arg IP "$IP" '.objects[]."global-domains"[] | "mgmt_cli -r true -d " + .name + " show objects ip-only true filter " + $IP + " --format json" ' >> search.sh
+mgmt_cli -r true show mdss details-level full --format json | jq -r --arg IP "$IP" '.objects[]."global-domains"[] | "mgmt_cli -r true -d " + .name + " show objects filter " + $IP + " --format json" ' >> search.sh
 chmod +x search.sh
-/bin/bash search.sh
+/bin/bash search.sh > $IP-domain_search.txt
 rm search.sh
+printf "\nResults of your search are in $IP-domain_search.txt\n"
