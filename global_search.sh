a505c9b9ae1584110d7674b36768034ccc3647b0
Adam Forester <aforester@silverdogstudios.com>
2020-02-20 09:00:41 -0600

    v2
    update on missed command

--- global_search.sh
+++ global_search.sh
@@ -5,6 +5,6 @@ mgmt_cli -r true show domains limit 500 --format json | jq --arg IP "$IP" -r '.o
 
 mgmt_cli -r true show mdss details-level full --format json | jq -r --arg IP "$IP" '.objects[]."global-domains"[] | "mgmt_cli -r true -d " + .name + " show objects filter " + $IP + " --format json" ' >> search.sh
 chmod +x search.sh
-/bin/bash search.sh > $IP-domain_search.txt
+/bin/bash search.sh >> $IP-domain_search.txt
 rm search.sh
 printf "\nResults of your search are in $IP-domain_search.txt\n"
