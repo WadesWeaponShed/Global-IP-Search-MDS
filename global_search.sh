5bbf71e23f719d2971f02f5e9b818adbeaf91da5
Adam Forester <aforester@silverdogstudios.com>
2020-02-20 09:12:41 -0600

    v2

--- global_search.sh
+++ global_search.sh
@@ -3,7 +3,7 @@ read IP
 
 mgmt_cli -r true show domains limit 500 --format json | jq --arg IP "$IP" -r '.objects[] | "mgmt_cli -r true -d " + .name + " show objects ip-only true filter " + $IP + " --format json" ' >> search.sh
 
-mgmt_cli -r true show mdss details-level full --format json | jq -r --arg IP "$IP" '.objects[]."global-domains"[] | "mgmt_cli -r true -d " + .name + " show objects filter " + $IP + " --format json" ' >> search.sh
+mgmt_cli -r true show mdss details-level full --format json | jq -r --arg IP "$IP" '.objects[]."global-domains"[] | "mgmt_cli -r true -d " + .name + " show objects filter " + $IP + " limit 500 --format json" ' >> search.sh
 chmod +x search.sh
 /bin/bash search.sh >> $IP-domain_search.txt
 rm search.sh
